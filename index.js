5c9b0e0412ccbd15ae1b16fee0a8a753f01db485
SeaHOH <Seahoh@Gmail.com>
2023-09-15 07:22:49 +0800

    feat: adjust the timing of replace
    set run at `document-start`, then could replace promptly after DOM
    loaded.
    
    prevent `for_you_feed` loading.
    
    add support Greasemonkey.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 // @name         Github Old Feed
 // @description  Replace the shit💩 new feed with the old one.
 // @author       荣顶
-// @version      1.6
+// @version      1.7
 // @license      MIT
-// @homepage      https://github.com/wangrongding/github-old-feed.git
-// @namespace    http://tampermonkey.net/
+// @homepage     https://github.com/wangrongding/github-old-feed.git
+// @namespace    https://github.com/wangrongding
+// @grant        none
+// @run-at       document-start
 // @match        https://github.com/
 // @match        https://github.com/dashboard
 // ==/UserScript==
@@ -13,27 +15,48 @@
 (function () {
   'use strict';
 
-  const feedContent = document.querySelector('.feed-content')
-  const feedMain = document.querySelector('.feed-main')
-  const sidebar = document.querySelector('.feed-right-sidebar')
-  if (feedContent) feedContent.style.maxWidth = "unset"
-  if (feedMain) feedMain.style.maxWidth = "100%"
-  if (sidebar) {
-    sidebar.style.maxWidth = "unset"
-    sidebar.style.width = "900px"
+  function fetchAndReplace() {
+    // Preserving the SSO container
+    const dashboard = document.querySelector('#dashboard feed-container');
+    if (!dashboard) return;
+    const loading = dashboard.querySelector(
+            'feed-container > div >' +
+            'include-fragment[src="/conduit/for_you_feed"] >' +
+            'div:first-child'
+          ) || document.createTextNode(
+            'Loading your activity, one moment please...'
+          );
+    // Stop for_you_feed as fast as possible
+    dashboard.replaceWith(loading);
+
+    const feedContent = document.querySelector('.feed-content');
+    const feedMain = document.querySelector('.feed-main');
+    const sidebar = document.querySelector('.feed-right-sidebar');
+    if (feedContent) feedContent.style.maxWidth = "unset";
+    if (feedMain) feedMain.style.maxWidth = "100%";
+    if (sidebar) {
+      sidebar.style.maxWidth = "unset";
+      sidebar.style.width = "900px";
+    }
+
+    fetch('https://github.com/dashboard-feed', {
+      // Smaller response
+      headers: {'x-requested-with': 'XMLHttpRequest' }
+    })
+      .then(response => response.text())
+      .then(text => {
+        const main = new DOMParser().parseFromString(text, 'text/html')
+                                    .querySelector('div[data-hpc]');
+        if (!main) throw('querySelector() failed');
+        loading.replaceWith(main);
+      })
+      .catch(error => {
+        console.error('Fetching the dashboard feed:', error);
+        loading.replaceWith(dashboard);
+      });
   }
 
-  fetch('https://github.com/dashboard-feed')
-    .then(response => response.text())
-    .then(text => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(text, 'text/html');
-      // Preserving the SSO container
-      const dashboard = document.querySelector("#dashboard feed-container");
-      const main = doc.querySelector('main');
-      if (dashboard && main) dashboard.replaceWith(main);
-    })
-    .catch(error => {
-      console.error('Fetching the dashboard feed:', error);
-    });
+  (document.readyState != 'loading') ? fetchAndReplace() :
+  document.addEventListener('readystatechange', fetchAndReplace, {once: true});
+
 })();
